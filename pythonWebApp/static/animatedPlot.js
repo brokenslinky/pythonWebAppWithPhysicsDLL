b9aaed47e8fbab9f64bdc80107020fe30dec05e7
Brock Palmer <brokenslinky@gmail.com>
2019-10-06 19:29:47 -0700

    2 decimal damping ratio

--- pythonWebApp/static/animatedPlot.js
+++ pythonWebApp/static/animatedPlot.js
@@ -4,7 +4,7 @@ var img = document.getElementById("myImage");
 
 var t = document.getElementById("myVar");
 setInterval(function () {
-    t.innerHTML = (parseFloat(t.innerHTML) + 0.01).toString();
+    t.innerHTML = (parseFloat(t.innerHTML) + 0.01).toFixed(2);
     img.src = "/plot.png?rate=49033&mass=272&ratio=" + t.innerHTML;
     return t.innerHTML;
-}, 2000);
+}, 1000);
